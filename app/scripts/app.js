e71e49db8b783ae5f597c1a9bbdd49f166f65db9
Nedson202 <simeonsamson4@gmail..com>
2018-06-10 16:40:59 +0100

    Add new files to boilerplate

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,25 @@
 module.exports = {
-    add: function(a,b) {
-        
-	return a + b
-
-    },
+    multiplesAdd: function(number) {
+        let counter,
+          arrayOfNumbers = [],
+          total = 0;
+      
+        //return all numbers in the range 0 - number to arrayOfNumbers
+        for (counter = 0; counter < number; counter++) {
+          arrayOfNumbers.push(counter);
+        }
+      
+        /*loop through the new arrayOfNumbers to get all multiples 
+        of 3 or 5 and add them together
+        */
+        for (counter = 3; counter < arrayOfNumbers.length; counter++) {
+          if (
+            arrayOfNumbers[counter] % 3 === 0 ||
+            arrayOfNumbers[counter] % 5 === 0
+          ) {
+            total += arrayOfNumbers[counter];
+          }
+        }
+        return total;
+    }     
 }
--- test/spec/spec.js
+++ test/spec/spec.js
@@ -1,13 +1,43 @@
 const app = require('../../app/scripts/app');
 
-describe('add', function() {
-    it('should return 2 when called', function() {
-        let result = app.add(1,1);
-        expect(result).toEqual(2);
+describe('Addition of multiples', function() {
+    it('should return 3 when called', function() {
+        let result = app.multiplesAdd(5);
+        expect(result).toEqual(3);
     });
 
-    it('should return less than 3', function() {
-        let result = app.add(1,1);
-        expect(result).toBeLessThan(3);
+    it('should return value less than 30', function() {
+        let result = app.multiplesAdd(15);
+        expect(result).toBeGreaterThan(30);
+    })
+
+    it('should return value greater than 100', function() {
+        let result = app.multiplesAdd(17);
+        expect(result).toBeGreaterThan(30);
+    })
+
+    it('should return value greater than 100', function() {
+        let result = app.multiplesAdd('17');
+        expect(result).toBeGreaterThan(30);
+    })
+
+    it('should return 45 if input is a string', function() {
+        let result = app.multiplesAdd('17djdjd');
+        expect(result).toEqual(0);
+    })
+
+    it('should return 45 if input is a string', function() {
+        let result = app.multiplesAdd('17');
+        expect(result).toEqual(60);
+    })
+
+    it('should return 0 if value is undefined', function() {
+        let result = app.multiplesAdd(undefined);
+        expect(result).toEqual(0);
+    })
+
+    it('should return 0 if value is null', function() {
+        let result = app.multiplesAdd(null);
+        expect(result).toEqual(0);
     })
 });
