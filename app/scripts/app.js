7a48dd27cd597240e19a71380508551061571212
Nedson202 <simeonsamson4@gmail..com>
2018-06-11 13:25:51 +0100

    Add more tests

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,25 +1,38 @@
 module.exports = {
-    multiplesAdd: function(number) {
-        let counter,
-          arrayOfNumbers = [],
-          total = 0;
-      
-        //return all numbers in the range 0 - number to arrayOfNumbers
-        for (counter = 0; counter < number; counter++) {
-          arrayOfNumbers.push(counter);
-        }
-      
-        /*loop through the new arrayOfNumbers to get all multiples 
-        of 3 or 5 and add them together
-        */
-        for (counter = 3; counter < arrayOfNumbers.length; counter++) {
-          if (
-            arrayOfNumbers[counter] % 3 === 0 ||
-            arrayOfNumbers[counter] % 5 === 0
-          ) {
-            total += arrayOfNumbers[counter];
-          }
-        }
-        return total;
-    }     
+  multiplesAdd: function(number) {
+    let counter,
+      arrayOfNumbers = [],
+      total = 0;
+  
+    //return all numbers in the range 0 - number to arrayOfNumbers
+    for (counter = 0; counter < number; counter++) {
+      arrayOfNumbers.push(counter);
+    }
+  
+    /*loop through the new arrayOfNumbers to get all multiples 
+    of 3 or 5 and add them together
+    */
+    for (counter = 3; counter < arrayOfNumbers.length; counter++) {
+      if (
+        arrayOfNumbers[counter] % 3 === 0 ||
+        arrayOfNumbers[counter] % 5 === 0
+      ) {
+        total += arrayOfNumbers[counter];
+      }
+    }
+    return total;
+  },
+
+  inputChecker: function(number) {
+    if(typeof number !== 'number') {
+      return 'Please enter a positive number'     
+    }
+
+    if(number <= 0) {
+      return 'Please enter a number greater than 0'     
+    }
+
+    return this.multiplesAdd(number);
+  }
 }
+
--- test/spec/spec.js
+++ test/spec/spec.js
@@ -2,42 +2,37 @@ const app = require('../../app/scripts/app');
 
 describe('Addition of multiples', function() {
     it('should return 3 when called', function() {
-        let result = app.multiplesAdd(5);
-        expect(result).toEqual(3);
+        let result = app.inputChecker(7);
+        expect(result).toEqual(14);
     });
 
     it('should return value less than 30', function() {
-        let result = app.multiplesAdd(15);
-        expect(result).toBeGreaterThan(30);
+        let result = app.inputChecker(15);
+        expect(result).toBeLessThan(100);
     })
 
     it('should return value greater than 100', function() {
-        let result = app.multiplesAdd(17);
-        expect(result).toBeGreaterThan(30);
-    })
-
-    it('should return value greater than 100', function() {
-        let result = app.multiplesAdd('17');
+        let result = app.inputChecker(17);
         expect(result).toBeGreaterThan(30);
     })
 
     it('should return 45 if input is a string', function() {
-        let result = app.multiplesAdd('17djdjd');
-        expect(result).toEqual(0);
+        let result = app.inputChecker('17djdjd');
+        expect(result).toEqual('Please enter a positive number');
     })
 
     it('should return 45 if input is a string', function() {
-        let result = app.multiplesAdd('17');
-        expect(result).toEqual(60);
+        let result = app.inputChecker(0);
+        expect(result).toEqual('Please enter a number greater than 0');
     })
 
-    it('should return 0 if value is undefined', function() {
-        let result = app.multiplesAdd(undefined);
-        expect(result).toEqual(0);
+    it('should return error if value is undefined', function() {
+        let result = app.inputChecker(undefined);
+        expect(result).toEqual('Please enter a positive number');
     })
 
-    it('should return 0 if value is null', function() {
-        let result = app.multiplesAdd(null);
-        expect(result).toEqual(0);
+    it('should return error if value is null', function() {
+        let result = app.inputChecker(null);
+        expect(result).toEqual('Please enter a positive number');
     })
 });
